811c70958aa9215e8191be2b13f6c16a27089fae
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-10 12:09:03 -0700

    chore: Create mockup for initial prompt form design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,10 @@
+import PromptGenMockup from './components/PromptGenMockup';
+
 function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex flex-col gap-8 h-screen w-screen items-center justify-center">
       <h1 className="text-7xl font-bold">Hello, Team 37!</h1>
+      <PromptGenMockup />
     </div>
   );
 }
