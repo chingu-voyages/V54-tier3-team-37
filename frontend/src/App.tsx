89b8c7f4ed458a982ad0eb0e3571f22ad41f90aa
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-10 11:15:21 -0700

    feat(styling): Add prettier-plugin-tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <h1 className="text-7xl font-bold">Hello, Team 37!</h1>
     </div>
   );
