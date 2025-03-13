3e5ee1979bcadf8d1dd3a82b4c29efae3038b42e
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-13 00:10:20 -0700

    feat(homepage): Change Footer root element tag to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="text-background w-full bg-neutral-700 p-16">
+    <footer className="text-background w-full bg-neutral-700 p-16">
       <p className="mx-auto max-w-xl text-center text-xl">WOW A FOOTER</p>
-    </div>
+    </footer>
   );
 };
 
