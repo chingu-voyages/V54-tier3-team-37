963e85252baac6b010c11caff2b41770f9c4439c
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-18 10:48:42 -0400

    update(backend/routes): Add routes for testing

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -3,6 +3,16 @@ import prisma from "../prisma.js";
 
 export const userRoute: Router = Router({});
 
+userRoute.get("/all", async (req: Request, res: Response) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
 userRoute.post("/", async (req: Request, res: Response) => {
   try {
     const { email, displayName } = req.body;
@@ -16,28 +26,29 @@ userRoute.post("/", async (req: Request, res: Response) => {
   }
 });
 
-// userRoute.get("/:userId", async (req: Request, res: Response) => {
-//     try {
-//         const { userId } = req.params;
-//
-//         // Validate userId
-//         // if (!isUUID(userId)) {
-//         //     return res.status(400).json({ error: "Invalid UUID format for userId" });
-//         // }
-//
-//         // Fetch the user and their prompts
-//         const user = await prisma.user.findUnique({
-//             where: { id: userId },
-//             include: { prompts: true }, // Include associated prompts
-//         });
-//
-//         if (!user) {
-//             return res.status(404).json({ error: "User not found" });
-//         }
-//
-//         res.status(200).json(user);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: "Something went wrong" });
-//     }
-// });
+userRoute.get("/:userId", async (req: Request, res: Response) => {
+  try {
+    const { userId } = req.params;
+
+    // Validate userId
+    // if (!isUUID(userId)) {
+    //     return res.status(400).json({ error: "Invalid UUID format for userId" });
+    // }
+
+    // Fetch the user and their prompts
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      include: { prompts: true }, // Include associated prompts
+    });
+
+    if (!user) {
+      res.status(404).json({ error: "User not found" });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
