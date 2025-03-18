f5cfcabd1c5104c6664813f50a97ad693f342d19
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-18 09:23:21 -0400

    update(backend/settings): Remove test code

--- backend/src/config/settings.ts
+++ backend/src/config/settings.ts
@@ -1,18 +1,13 @@
 import express from "express";
-// import { userRoute } from "../routes/userRoutes.ts";
-// import { promptRoute } from "../routes/promptRoutes.ts";
+import { userRoute, promptRoute } from "../routes/index.ts";
 
 export const configApp = () => {
   const app = express();
 
   app.use(express.json());
 
-  app.get("/", (req, res) => {
-    res.send("Hello, world!");
-  });
-
-  //   app.use("/users", userRoute);
-  //   app.use("/prompts", promptRoute);
+  app.use("/users", userRoute);
+  app.use("/prompts", promptRoute);
 
   return app;
 };
