11533102135ce3f7e1e89ee7ea2daa8db401cb0a
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-18 08:58:16 -0400

    fix(backend/server): Update import extentions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,24 +1,24 @@
-import {configApp} from "./config/settings.js";
-import prisma from "./prisma.js";
+import prisma from "./prisma.ts";
+import { configApp } from "./config/settings.ts";
+// Enable access to environment variables
+import "dotenv/config";
 
 const port = process.env.PORT || 3000;
 
-export const app = configApp()
-console.log("index.ts")
-
+export const app = configApp();
 
 const startApp = async (): Promise<void> => {
-    try {
-        await prisma.$connect();
-        console.log("Connected to Database");
+  try {
+    await prisma.$connect();
+    console.log("Connected to Database");
 
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`);
-        });
-    } catch (error) {
-        console.error("Error starting the app:", error);
-        process.exit(1);
-    }
-}
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting the app:", error);
+    process.exit(1);
+  }
+};
 
 startApp();
