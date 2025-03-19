eebb818362bfb558c9bb50621445350a8b1bd758
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-03-18 22:33:30 -0300

    feat(backend): added endpoints for userRoute for testing connection

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -16,29 +16,26 @@ userRoute.post("/", async (req, res) => {
     }
 });
 
-// userRoute.get("/:userId", async (req: Request, res: Response) => {
-//     try {
-//         const { userId } = req.params;
-//
-//         // Validate userId
-//         // if (!isUUID(userId)) {
-//         //     return res.status(400).json({ error: "Invalid UUID format for userId" });
-//         // }
-//
-//         // Fetch the user and their prompts
-//         const user = await prisma.user.findUnique({
-//             where: { id: userId },
-//             include: { prompts: true }, // Include associated prompts
-//         });
-//
-//         if (!user) {
-//             return res.status(404).json({ error: "User not found" });
-//         }
-//
-//         res.status(200).json(user);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({ error: "Something went wrong" });
-//     }
-// });
+userRoute.get("/:userId", async (req: Request, res: Response) => {
+    try {
+        const { userId } = req.params;
+
+        const user = await prisma.user.findUnique({
+            where: { id: userId },
+            include: { prompts: true },
+        });
+
+        if (!user) {
+            res.status(404).json({ error: "User not found" });
+            return
+        }
+
+        res.status(200).json(user);
+        return
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Something went wrong" });
+    }
+});
 
