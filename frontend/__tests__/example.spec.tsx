4187d9bff464871c389ae17358e7cc8f4693df50
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-20 02:09:07 -0700

    fun(tests): Change text of example.spec.tsx to "Hello World!"

--- frontend/__tests__/example.spec.tsx
+++ frontend/__tests__/example.spec.tsx
@@ -6,7 +6,7 @@ import { render } from '@testing-library/react';
 
 describe('example', () => {
   it('should render', () => {
-    const { getByText } = render(<div>hello</div>);
-    expect(getByText('hello')).toBeInTheDocument();
+    const { getByText } = render(<div>Hello World!</div>);
+    expect(getByText('Hello World!')).toBeInTheDocument();
   });
 });
