c158a8f119dcdb0316d8fd99dc91bf5883a9935c
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-20 01:40:43 -0700

    feat(homepage): Add AboutHow and adjust text color

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -11,10 +11,11 @@ import { Separator } from './ui/separator';
 
 const About = () => {
   return (
-    <div className="w-full">
+    <div className="w-full text-muted-foreground">
       <AboutWhat />
       <AboutFeatures />
       <AboutWhy />
+      <AboutHow />
     </div>
   );
 };
@@ -46,20 +47,23 @@ const AboutWhat = () => {
       <Container className="gap-32 pt-16 pb-32">
         <div>
           <h2 className="text-3xl font-bold uppercase">Welcome to Prompto</h2>
-          <Separator className="border-foreground mx-auto mt-4 max-w-1/2 border-2" />
+          <Separator className="border-muted-foreground mx-auto mt-4 max-w-1/2 border-2" />
         </div>
         <div className="grid grid-cols-2 tracking-tighter">
-          <div className="grid w-fit h-fit grid-cols-2 text-center tracking-tighter">
+          <div className="grid h-fit w-fit grid-cols-2 text-center tracking-tighter">
             {personas.map((persona, index) => (
               <div
                 key={persona.name}
-                className={cn(`bg-muted size-40 flex flex-col justify-end p-8`, index % 2 === 0 && 'self-end mt-8 mr-8')}
+                className={cn(
+                  `bg-muted flex size-40 flex-col justify-end p-8`,
+                  index % 2 === 0 && 'mt-8 mr-8 self-end'
+                )}
               >
                 <span className="text-xl">{persona.name}</span>
               </div>
             ))}
           </div>
-          <div className="flex flex-col gap-8">
+          <div className="space-y-8">
             <h3 className="text-2xl font-bold">What is Prompto?</h3>
             <p className="text-lg leading-loose">
               Prompto is an advanced <strong>AI prompt generator</strong> designed to streamline the
@@ -114,7 +118,7 @@ const AboutFeatures = () => {
       <Container className="gap-24 pt-16 pb-48">
         <div>
           <h2 className="text-3xl font-bold">Key Features</h2>
-          <Separator className="border-foreground mx-auto mt-4 max-w-1/2 border-2" />
+          <Separator className="border-muted-foreground mx-auto mt-4 max-w-1/2 border-2" />
         </div>
         <div className="grid h-112 w-full grid-cols-4 gap-8 text-center tracking-tighter">
           {cards.map((card, index) => (
@@ -139,15 +143,17 @@ const AboutWhy = () => {
   const reasons = [
     {
       heading: 'Simplified Prompt Creation',
-      description: 'No more guessing. Generate prompts that align with your goals instantly.'
+      description: 'No more guessing. Generate prompts that align with your goals instantly.',
     },
     {
       heading: 'Consistency & Quality',
-      description: 'Ensure your AI\'s outputs are predictable, relevant, and of high quality, every time.'
+      description:
+        "Ensure your AI's outputs are predictable, relevant, and of high quality, every time.",
     },
     {
       heading: 'Save Time & Boost Productivity',
-      description: 'Focus on what matters most—innovation—while we handle the prompt crafting for you.'
+      description:
+        'Focus on what matters most—innovation—while we handle the prompt crafting for you.',
     },
   ];
 
@@ -156,10 +162,10 @@ const AboutWhy = () => {
       <Container className="gap-32 pt-16 pb-32">
         <div>
           <h2 className="text-3xl font-bold uppercase">Why Choose Our AI Prompt Generator</h2>
-          <Separator className="border-foreground mx-auto mt-4 max-w-1/2 border-2" />
+          <Separator className="border-muted-foreground mx-auto mt-4 max-w-1/2 border-2" />
         </div>
-        <div className="grid grid-cols-2 tracking-tighter gap-32">
-          <div className="flex flex-col gap-16 tracking-tighter">
+        <div className="grid grid-cols-2 gap-32 tracking-tighter">
+          <div className="space-y-16 tracking-tighter">
             {reasons.map((reason) => (
               <div
                 key={reason.heading}
@@ -170,9 +176,186 @@ const AboutWhy = () => {
               </div>
             ))}
           </div>
-          <div className="h-full w-full bg-muted">
-          </div>
+          <div className="bg-muted h-full w-full"></div>
         </div>
+      </Container>
+    </section>
+  );
+};
+
+const AboutHow = () => {
+  const reasons = [
+    {
+      heading: 'Simplified Prompt Creation',
+      description: 'No more guessing. Generate prompts that align with your goals instantly.',
+    },
+    {
+      heading: 'Consistency & Quality',
+      description:
+        "Ensure your AI's outputs are predictable, relevant, and of high quality, every time.",
+    },
+    {
+      heading: 'Save Time & Boost Productivity',
+      description:
+        'Focus on what matters most—innovation—while we handle the prompt crafting for you.',
+    },
+  ];
+
+  const steps = [
+    {
+      step: 1,
+      name: 'Login or Sign Up',
+      items: [
+        {
+          heading: '',
+          description: (
+            <p>
+              Login to your existing account or Sign Up if you don't have one yet. This will give
+              you access to the prompt generation features.
+            </p>
+          ),
+        },
+      ],
+    },
+    {
+      step: 2,
+      name: 'Fill in the form',
+      items: [
+        {
+          heading: 'Persona',
+          description: (
+            <p className="inline">
+              Define the role or identity you want the AI model to adopt (e.g. "UX Designer",
+              "Content Writer"). This helps guide the tone and style of the response.
+            </p>
+          ),
+        },
+        {
+          heading: 'Context',
+          description: (
+            <p className="inline">
+              Provide background information relevant to the task. This ensures the AI understands
+              the situation or specific task at hand.
+            </p>
+          ),
+        },
+        {
+          heading: 'Task',
+          description: (
+            <p className="inline">
+              Clearly specify the action or output you want from the AI model. Be direct and concise
+              about what you need.
+            </p>
+          ),
+        },
+        {
+          heading: 'Output',
+          description: (
+            <p className="inline">
+              Detail the format, style, and tone of the AI's response. Do you need a creative piece,
+              a formal report, or a technical document?
+            </p>
+          ),
+        },
+        {
+          heading: 'Constraints',
+          description: (
+            <p className="inline">
+              Set any limitations or guidelines the AI must follow, such as accessibility
+              requirements, word count limits, or content restrictions.
+            </p>
+          ),
+        },
+      ],
+    },
+    {
+      step: 3,
+      name: 'Click "Generate Prompt"',
+      items: [
+        {
+          heading: '',
+          description: (
+            <p>
+              Once all fields are filled, click, the <strong>"Generate Prompt"</strong> button to
+              generate your custom prompt based on the input provided.
+            </p>
+          ),
+        },
+      ],
+    },
+    {
+      step: 4,
+      name: 'View the result',
+      items: [
+        {
+          heading: 'Copy',
+          description: (
+            <p className="inline">
+              The generated prompt will appear in the <strong>"Result"</strong> section. You can
+              copy it by selecting the text and using <strong>Ctrl+C (Cmd+C)</strong>or the{' '}
+              <strong>"Copy" button.</strong>
+            </p>
+          ),
+        },
+        {
+          heading: 'Save for Future Use',
+          description: (
+            <p className="inline">
+              If you plan to use the prompt later, save it to your device, or store it in the app
+              for quick access.
+            </p>
+          ),
+        },
+        {
+          heading: 'Regenerate',
+          description: (
+            <p className="inline">
+              If you're not satisfied with the result, click "Regenerate" to create a new version of
+              the prompt with potentially different output.
+            </p>
+          ),
+        },
+      ],
+    },
+  ];
+
+  return (
+    <section className="w-full">
+      <Container className="gap-32 pt-16 pb-32">
+        <div>
+          <h2 className="text-3xl font-bold uppercase">How to Use Prompto's AI Prompt Generator</h2>
+          <Separator className="border-muted-foreground mx-auto mt-4 max-w-1/2 border-2" />
+        </div>
+
+        <ol className="space-y-16 text-lg">
+          {steps.map((step) => (
+            <li
+              key={step.step}
+              className="grid grid-cols-2 justify-between gap-32 tracking-tighter"
+            >
+              <div className="space-y-4">
+                <h3 className="font-bold">
+                  Step {step.step}: {step.name}
+                </h3>
+                {step.items.length > 1 ? (
+                  <ul className="list-disc space-y-4">
+                    {step.items.map((item) => (
+                      <li
+                        key={item.heading}
+                        className="ml-8"
+                      >
+                        <h4 className="inline font-bold">{item.heading}</h4>: {item.description}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  step.items[0].description
+                )}
+              </div>
+              <div className="bg-muted mt-12 h-full w-full"></div>
+            </li>
+          ))}
+        </ol>
       </Container>
     </section>
   );
