2ad9613fb4227a8336a78ec0cc9314492296b7a2
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-03-22 02:30:59 -0700

    feat(homepage): Update team member names and add links to socials

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -11,50 +11,50 @@ const teamMembers: TeamMember[] = [
   {
     name: 'Sarita Jha',
     role: 'Product Owner',
-    linkedin: '',
+    linkedin: 'https://www.linkedin.com/in/jha-sarita/',
     github: '',
   },
   {
-    name: 'Xoch',
+    name: 'Xochitl Farias',
     role: 'Scrum Master',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/xfarias-scrum-master/',
+    github: 'https://github.com/xochfa',
   },
   {
-    name: 'Stefley',
+    name: 'Steffi Saint-Pierre',
     role: 'Scrum Master',
-    linkedin: '',
+    linkedin: 'https://www.linkedin.com/in/steffisp/',
     github: '',
   },
   {
-    name: 'Trupti',
+    name: 'Trupti Shikhare',
     role: 'UI/UX Designer',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/truptishikhare/',
+    github: 'https://github.com/truptishikhare',
   },
   {
     name: 'Aigul',
     role: 'Web Developer',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/aigul-ermak/',
+    github: 'https://github.com/aigul-ermak',
   },
   {
-    name: 'Nika',
+    name: 'Nika Kolesnikova',
     role: 'Web Developer',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/vekolesnikova/',
+    github: 'https://github.com/kweeuhree',
   },
   {
-    name: 'Luis',
+    name: 'Luis Castillo',
     role: 'Web Developer',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/luis-castillokc/',
+    github: 'https://github.com/LuisCastilloKC',
   },
   {
     name: 'Brendan K. Schatzki',
     role: 'Web Developer',
-    linkedin: '',
-    github: '',
+    linkedin: 'https://www.linkedin.com/in/bkschatzki/',
+    github: 'https://github.com/BKSchatzki',
   },
 ];
 
@@ -65,7 +65,7 @@ const Footer = () => {
     <footer className="text-background bg-muted-foreground w-full py-16">
       <Container className="flex-row justify-between">
         <div className="flex items-center gap-4">
-          <span className="text-7xl font-keania-one lowercase">Prompto</span>
+          <span className="font-keania-one text-7xl lowercase">Prompto</span>
         </div>
         <ul className="flex flex-col gap-4 text-lg">
           {roles.map((role) => (
@@ -73,16 +73,28 @@ const Footer = () => {
               key={role}
               className="flex justify-between gap-8"
             >
-              <span className="shrink-0 min-w-32 text-right">{role}</span>
+              <span className="min-w-32 shrink-0 text-right">{role}</span>
               <ul className="w-full">
                 {teamMembers.map((member) => (
                   <li key={member.name}>
                     {member.role === role ? (
-                      <div className="flex gap-8 justify-between items-center">
+                      <div className="flex items-center justify-between gap-8">
                         <span>{member.name}</span>
                         <div className="flex gap-4">
-                          <Linkedin size={18 /* default: 24 */} />
-                          <Github size={18 /* default: 24 */} />
+                          <a
+                            href={member.linkedin || ''}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                          >
+                            <Linkedin size={18 /* default: 24 */} />
+                          </a>
+                          <a
+                            href={member.github || ''}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                          >
+                            <Github size={18 /* default: 24 */} />
+                          </a>
                         </div>
                       </div>
                     ) : null}
