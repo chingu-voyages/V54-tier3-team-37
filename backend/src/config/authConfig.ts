05cbd776356e956104ff1588844b112569b1af46
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-25 09:01:46 -0400

    update(backend/config): Update Google Auth redirect url to a deployed link

--- backend/src/config/authConfig.ts
+++ backend/src/config/authConfig.ts
@@ -12,7 +12,8 @@ export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
 
-export const GOOGLE_CALLBACK_URL = "http://localhost:8000/google-callback";
+export const GOOGLE_CALLBACK_URL =
+  "https://v54-tier3-team-37.onrender.com/google-callback";
 export const GOOGLE_OAUTH_SCOPES = [
   "https://www.googleapis.com/auth/userinfo.email",
   "https://www.googleapis.com/auth/userinfo.profile",
