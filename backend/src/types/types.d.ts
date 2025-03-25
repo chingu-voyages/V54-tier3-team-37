44cf2823065f4ea51af7ce306cfeec1c58982825
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-25 18:54:56 -0400

    feat(backend/types): Add userId type to global Request object

--- backend/src/types/types.d.ts
+++ backend/src/types/types.d.ts
@@ -1,6 +1,15 @@
-// this type declaration extends default express-session types
+// Extend default express-session types
 import { Session } from "express-session";
 
 export interface GSession extends Session {
   [key: string]: string;
+}
+
+// Extend request interface
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
 }
