fc720208454ddbf6e1474ff988e14eb481e78fae
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-25 17:52:47 -0400

    feat(backend/controllers): Add logic that will check whether a user exists in a db or create a new one

---
+++ backend/src/controllers/findOrCreateUserId.ts
@@ -0,0 +1,28 @@
+import prisma from "../prisma.ts";
+import { UserPayload } from "../types/userTypes.ts";
+
+export const findOrCreateUserId = async (
+  user: UserPayload
+): Promise<string> => {
+  const existingUser = await prisma.user.findUnique({
+    where: { email: user.email },
+  });
+  if (existingUser) return existingUser.id;
+
+  // Save user image
+  // -- I thought that google would be more popular, so google option is first
+  // const userImage = user.picture ?? user.avatar_url ?? null;
+
+  const newUser = await prisma.user.create({
+    data: { email: user.email, displayName: user.displayName }, // need to add a field for image
+  });
+  return newUser.id;
+};
+
+export const findUserById = async (userId: string) => {
+  const user = await prisma.user.findUnique({
+    where: { id: userId },
+  });
+
+  if (user) return user.id;
+};
