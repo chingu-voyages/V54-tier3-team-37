91479877407d3495159642a35067945a261515e6
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-26 12:21:10 -0400

    test(backend/middleware): Add a preliminary test

---
+++ backend/__tests__/db/findOrCreateUserId.test.ts
@@ -0,0 +1,57 @@
+import { prismaMock } from "../../src/prismaSingleton";
+import { findOrCreateUserId } from "../../src/controllers/findOrCreateUser";
+import { mockUsers } from "../../__mocks__/mockUsers";
+
+describe("findOrCreateUserId", () => {
+  test("returns an existing user id if user exists", async () => {
+    const validMockUsers = mockUsers.validUser;
+    for (const mockUser of validMockUsers) {
+      // await prisma.user.delete({
+      //   where: { email: mockUser.email },
+      // });
+      let newUser;
+      try {
+        newUser = await prismaMock.user.create({
+          data: {
+            email: mockUser.email,
+            displayName: mockUser.displayName,
+          },
+        });
+        if (!newUser) throw Error("failed to create a test user");
+        const userId = await findOrCreateUserId({
+          email: mockUser.email,
+          displayName: mockUser.displayName,
+        });
+        expect(userId).toBe(newUser.id);
+        const deleted = await prismaMock.user.delete({
+          where: { id: newUser.id },
+        });
+        expect(deleted.id).toBe(newUser.id);
+      } catch (error) {
+        throw new Error(error.message ? error.message : String(error));
+      }
+    }
+  });
+
+  test("creates a new user and returns their id if user does not exist", async () => {
+    const validMockUsers = mockUsers.unregisteredUser;
+    for (const mockUser of validMockUsers) {
+      try {
+        const userId = await findOrCreateUserId({
+          email: mockUser.email,
+          displayName: mockUser.displayName,
+        });
+
+        expect(userId).toBeDefined();
+
+        const deleted = await prismaMock.user.delete({
+          where: { id: userId },
+        });
+
+        expect(deleted.id).toBe(userId);
+      } catch (error) {
+        throw new Error(error.message ? error.message : String(error));
+      }
+    }
+  });
+});
