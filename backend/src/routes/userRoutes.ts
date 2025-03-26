7be6047b7ea2e278e1fd103acbd4bf8f3ead741c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-03-25 22:56:04 -0300

    refactor(routes): add comment

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
-import {Router, RequestHandler} from "express";
+import {RequestHandler, Router} from "express";
 
 import {userController} from "../controllers/index.js";
 
 
 export const userRoute: Router = Router({});
 
+// GET /users/:userId → Fetch user profile by ID
 userRoute.get("/:userId", userController.getUserProfile as RequestHandler);
 
 
