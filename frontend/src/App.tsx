09020a83382acc9c52377740e4d23821e9c752c5
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-27 08:36:28 -0400

    fix(merge): Commit after resolving merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,56 +1,33 @@
-<<<<<<< HEAD
-import { Routes, Route } from 'react-router-dom';
-=======
 import { Provider } from 'react-redux';
-import {
-  Route,
-  Routes,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
 import About from './components/About';
 import AboutPage from './components/AboutPage';
 import Auth from './components/Auth';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Nav from './components/Nav';
-<<<<<<< HEAD
-import Auth from './components/Auth';
-import AboutPage from './components/AboutPage';
-=======
 import { store } from './store';
-
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
 // import PromptGenMockup from './components/PromptGenMockup';
 
 function App() {
   // let user;
 
   return (
-<<<<<<< HEAD
-    <div className="flex h-screen w-screen flex-col items-center justify-start">
-      <header className="w-full">
-        <Nav />
-      </header>
-=======
     <Provider store={store}>
       <div className="flex h-screen w-screen flex-col items-center justify-start">
         <header className="w-full">
           <Nav />
         </header>
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-<<<<<<< HEAD
-              <Hero />
-              {/* {user ? (
-=======
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
                 <Hero />
                 {/* {user ? (
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
+                <Hero />
+                {/* {user ? (
         <PromptGenMockup />
       ) : (
         <div className="bg-muted w-full p-16">
@@ -59,32 +36,23 @@ function App() {
           </p>
         </div>
       )} */}
-<<<<<<< HEAD
-              <About />
-=======
                 <About />
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
-            </>
-          }
-        />
-        <Route
-          path="/about"
-          element={<AboutPage />}
-        />
-        <Route
-          path="/auth"
-          element={<Auth />}
-        />
-      </Routes>
+              </>
+            }
+          />
+          <Route
+            path="/about"
+            element={<AboutPage />}
+          />
+          <Route
+            path="/auth"
+            element={<Auth />}
+          />
+        </Routes>
 
-<<<<<<< HEAD
-      <Footer />
-    </div>
-=======
         <Footer />
       </div>
     </Provider>
->>>>>>> 84784c98258d97c6b01c3a30924fb3ed6d12cbd6
   );
 }
 
