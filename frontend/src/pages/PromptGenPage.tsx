8253a394b89df80e8e88261a8744aa68ccde4e18
Luis Castillo <mr.luiscastillo@gmail.com>
2025-03-28 01:26:00 -0400

    move promptGenPage to a page folder

---
+++ frontend/src/pages/PromptGenPage.tsx
@@ -0,0 +1,11 @@
+import PromptGenMockup from '@/components/PromptGenMockup';
+
+const PromptGenPage = () => {
+  return (
+    <div className="flex w-full items-center justify-center px-4 py-10">
+      <PromptGenMockup />
+    </div>
+  );
+};
+
+export default PromptGenPage;
