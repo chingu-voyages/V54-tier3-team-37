79723c38084479dfb7a671cb341abc2c3538b129
Nika Kolesnikova <firstnamenika@gmail.com>
2025-03-31 11:16:43 -0400

    update(backend/swagger): Add a path to yaml docs

--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,39 +1,43 @@
-import swaggerJsdoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import {Express} from 'express';
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
 
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Prompto API',
-            version: '1.0.0',
-            description: 'Swagger documentation for Prompto project',
-        },
-        components: {
-            securitySchemes: {
-                cookieAuth: {
-                    type: 'apiKey',
-                    in: 'cookie',
-                    name: 'token',
-                },
-            },
-        },
-        security: [
-            {
-                cookieAuth: [],
-            },
-        ],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Prompto API",
+      version: "1.0.0",
+      description: "Swagger documentation for Prompto project",
     },
-    apis: isProd ? ['./dist/routes/**/*.js'] : ['./src/routes/**/*.ts'],
+    components: {
+      securitySchemes: {
+        cookieAuth: {
+          type: "apiKey",
+          in: "cookie",
+          name: "token",
+        },
+      },
+    },
+    security: [
+      {
+        cookieAuth: [],
+      },
+    ],
+  },
+  apis: [
+    ...(isProd
+      ? ["./dist/docs/**/*.yaml", "./dist/routes/**/*.js"]
+      : ["./src/docs/**/*.yaml", "./src/routes/**/*.ts"]),
+  ],
 };
 
 const swaggerSpec = swaggerJsdoc(options) as {
-    paths: Record<string, unknown>;
+  paths: Record<string, unknown>;
 };
 
 export function setupSwagger(app: Express): void {
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
