10c895157e7db4e603ce726a47eb663b3c5c1027
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-04-01 02:38:29 -0700

    feat(history): Scaffold blank history page

--- frontend/src/components/PromptHistory.tsx
+++ frontend/src/components/PromptHistory.tsx
@@ -1,4 +1,9 @@
 import {
+  useEffect,
+  useState,
+} from 'react';
+
+import {
   Plus,
   Triangle,
 } from 'lucide-react';
@@ -13,8 +18,17 @@ import {
 } from './ui/dropdown-menu';
 
 const PromptHistory = () => {
+  const [promptList, setPromptList] = useState([]);
+
+  useEffect(() => {
+    const epicPromptListSetterOmg = () => {
+      setPromptList([])
+    }
+    epicPromptListSetterOmg();
+  }, [])
+
   return (
-    <div className="flex w-full flex-col gap-16">
+    <div className="flex w-full flex-col gap-16 pb-16">
       <div className="flex items-center justify-between gap-4">
         <DropdownMenu>
           <DropdownMenuTrigger className="bg-muted flex items-center justify-between gap-2 rounded-md border px-4 py-1">
@@ -29,15 +43,33 @@ const PromptHistory = () => {
             <DropdownMenuItem>Rating</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-        <Button>
-          <Link
-            to="../generate"
-            className="flex items-center justify-between gap-2"
-          >
-            <Plus /> Generate New Prompt
-          </Link>
-        </Button>
+        {promptList.length > 0 &&
+          <Button size="lg">
+            <Link
+              to="../generate"
+              className="flex items-center justify-between gap-2"
+            >
+              <Plus /> Generate New Prompt
+            </Link>
+          </Button>
+        }
       </div>
+      {promptList.length < 1 &&
+        <div className="flex flex-col gap-16 items-center">
+          <div className="flex flex-col gap-8 items-center">
+            <div className="size-48 bg-muted-foreground rounded-full"></div>
+            <p className="text-muted-foreground">You don't have any saved prompts to review.</p>
+          </div>
+          <Button size="lg">
+            <Link
+              to="../generate"
+              className="flex items-center justify-between gap-2"
+            >
+              <Plus /> Generate New Prompt
+            </Link>
+          </Button>
+        </div>
+      }
     </div>
   );
 };
