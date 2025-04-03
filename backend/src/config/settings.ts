3b61e5f3821c1c1d7c91218e55d534dc8ac84dc0
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-03 13:26:04 -0400

    feat(backend/config): Apply rate limiting middleware to all requests

--- backend/src/config/settings.ts
+++ backend/src/config/settings.ts
@@ -2,7 +2,8 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import {authRoute, promptRoute, userRoute} from "../routes/index.js";
+import { authRoute, promptRoute, userRoute } from "../routes/index.js";
+import { limiter } from "../middleware/rateLimiter.js";
 import cors from "cors";
 
 type NodeEnv = "development" | "production" | "test";
@@ -10,37 +11,42 @@ type NodeEnv = "development" | "production" | "test";
 const nodeEnv = process.env.NODE_ENV as NodeEnv;
 
 export const configApp = async () => {
-    const app = express();
-    app.use(express.json());
+  const app = express();
+  app.use(express.json());
 
-    if (nodeEnv !== "test") {
-        const {setupSwagger} = await import("../swagger.js");
-        setupSwagger(app);
-    }
+  if (nodeEnv !== "test") {
+    const { setupSwagger } = await import("../swagger.js");
+    setupSwagger(app);
+  }
 
-    const allowedOrigins = process.env.HOME_REACT_ADDRESS?.split(',') || ['http://localhost:5173'];
+  const allowedOrigins = process.env.HOME_REACT_ADDRESS?.split(",") || [
+    "http://localhost:5173",
+  ];
 
-    app.use(cookieParser());
-    app.use(
-        cors({
-            origin: allowedOrigins,
-            credentials: true,
-        })
-    );
+  app.use(cookieParser());
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      credentials: true,
+    })
+  );
 
-    // Serve static files
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, "..", "static")));
+  // Serve static files
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "..", "static")));
 
-    const sessionSecret = String(process.env.SESSION_SECRET);
-    app.use(
-        session({secret: sessionSecret, resave: false, saveUninitialized: true})
-    );
-    app.use(express.json());
+  // Apply the rate limiting middleware to all requests
+  app.use(limiter);
 
-    app.use("/", authRoute);
-    app.use("/users", userRoute);
-    app.use("/prompts", promptRoute);
+  const sessionSecret = String(process.env.SESSION_SECRET);
+  app.use(
+    session({ secret: sessionSecret, resave: false, saveUninitialized: true })
+  );
+  app.use(express.json());
 
-    return app;
+  app.use("/", authRoute);
+  app.use("/users", userRoute);
+  app.use("/prompts", promptRoute);
+
+  return app;
 };
