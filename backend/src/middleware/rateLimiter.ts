e673fcaac5a3d8932566fc8ddb0f32defa09c4bb
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-03 13:51:43 -0400

    update(backend/middleware): Limit the requests to 60 per minute

--- backend/src/middleware/rateLimiter.ts
+++ backend/src/middleware/rateLimiter.ts
@@ -1,9 +1,10 @@
 import { rateLimit } from "express-rate-limit";
 
+// This rate limiter is using the in-memory store solution
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  windowMs: 60 * 1000, // 1 minute
+  limit: 60, // Limit each IP to 100 requests per `window` (here, per 1 minutes).
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  // store: ... , // Redis, Memcached, etc. See below.
+  message: JSON.stringify("Too many requests. Try again in a minute."),
 });
