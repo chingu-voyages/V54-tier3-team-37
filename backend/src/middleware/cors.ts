4c88c5f55d74e65133a46605365a7f8a5505ec26
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-08 10:32:06 -0400

    update(backend/cors): Handle origins dynamically to ensure correct parsing and add more custom headers

--- backend/src/middleware/cors.ts
+++ backend/src/middleware/cors.ts
@@ -1,9 +1,20 @@
+import { CorsOptions } from "cors";
 import { NextFunction, Request, Response } from "express";
 
 const allowedOrigins = process.env.HOME_REACT_ADDRESS?.split(",");
 
-export const corsOptions = {
-  origin: allowedOrigins,
+export const corsOptions: CorsOptions = {
+  // Handle origins dynamically
+  origin: (
+    origin: string | undefined,
+    callback: (err: Error | null, allow?: boolean) => void
+  ) => {
+    if (!origin || allowedOrigins?.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Origin restricted by CORS"));
+    }
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
   allowedHeaders: ["Content-Type", "Cookie", "Set-Cookie", "Authorization"],
@@ -15,17 +26,22 @@ export const securityHeaders = (
   res: Response,
   next: NextFunction
 ) => {
+  // Accept partitioned cookies
+  res.header("Accept-CH", "Sec-CH-Partitioned-Cookies");
   // Allow cross-origin access to resources
-  res.header("Cross-Origin-Resource-Policy", "cross-origin");
+  res.header("Cross-Origin-Resource-Policy", "same-site");
   // Limit tracking data leakage
   res.header("Referrer-Policy", "strict-origin-when-cross-origin");
   // Reduces cookie blocking in Chromium-based privacy browsers
-  res.header("Permissions-Policy", "interest-cohort=()");
+  // Browsing topics for FireFox
+  res.header("Permissions-Policy", "interest-cohort=(), browsing-topics=()");
   // Restricts all resource loading to same-origin by default
   res.header("Content-Security-Policy", "default-src 'self'");
   // Disables MIME type sniffing
   res.header("X-Content-Type-Options", "nosniff");
   // Prevent a web page from being displayed in a frame or iframe, regardless of the origin
   res.header("X-Frame-Options", "deny");
+  // Ensure Render doesn't modify headers
+  res.header("Cache-Control", "no-transform");
   next();
 };
