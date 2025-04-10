1d934b2edd26ea18190a17d5b81c308675d9ddb2
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-10 12:45:28 -0400

    update(backend): Update app.listen message

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,25 +2,22 @@ import "dotenv/config.js";
 import env from "./env.js";
 import prisma from "./prisma.js";
 
-
-import {configApp} from "./config/index.js";
+import { configApp } from "./config/index.js";
 
 const port = env.PORT || 4000;
 
-
 const startApp = async (): Promise<void> => {
-    try {
+  try {
+    const app = await configApp();
+    await prisma.$connect();
 
-        const app = await configApp();
-        await prisma.$connect();
-
-        app.listen(port, () => {
-            console.log(`Example app listening on port ${port}`);
-        });
-    } catch (error) {
-        console.error("Error starting the app:", error);
-        process.exit(1);
-    }
+    app.listen(port, () => {
+      console.info(`Prompto listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error("Error starting the app:", error);
+    process.exit(1);
+  }
 };
 
 startApp();
