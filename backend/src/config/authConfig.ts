b632e2b7e8eb3ea85b45c687b97aab630a387048
Nika Kolesnikova <firstnamenika@gmail.com>
2025-04-14 18:50:45 -0400

    fix redirect urls

--- backend/src/config/authConfig.ts
+++ backend/src/config/authConfig.ts
@@ -6,14 +6,14 @@ export const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 
 export const GITHUB_REDIRECT_URL = `https://github.com/login/oauth/authorize?`;
 export const GITHUB_CALLBACK_URL =
-  "https://v54-tier3-team-37.onrender.com/auth/github-callback";
+  "https://api.prompto37.com/auth/github-callback";
 
 // Google OAuth variables
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 export const GOOGLE_CALLBACK_URL_DEV = "http://localhost:8000/google-callback";
 export const GOOGLE_CALLBACK_URL_PROD =
-  "https://v54-tier3-team-37.onrender.com/google-callback";
+  "https://api.prompto37.com/google-callback";
 
 export const GOOGLE_CALLBACK_URL = isProduction
   ? GOOGLE_CALLBACK_URL_PROD
