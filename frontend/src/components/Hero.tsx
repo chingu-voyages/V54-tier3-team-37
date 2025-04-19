d5a14a0b9081ef8c8e374365a8f99689fb11b7b2
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-04-18 21:00:47 -0700

    fix(hero): Fix hero image overflowing into first about section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -19,10 +19,10 @@ const Hero = () => {
   };
 
   return (
-    <section className="relative flex h-screen min-h-fit w-full flex-col text-center text-white sm:min-h-screen">
+    <section className="relative flex h-screen min-h-fit w-full flex-col text-center text-white sm:max-h-screen">
       <img
         src="/hero-image.png"
-        className="absolute top-0 left-0 -z-10 min-h-full min-w-full object-cover"
+        className="absolute top-0 left-0 -z-10 min-w-full object-cover max-sm:min-h-full sm:max-h-full"
       />
       <header className="w-full">
         <Nav />
